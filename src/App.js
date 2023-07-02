a609ab8f9196f56999e3b19256f8328e63d2b859
JanaHeyn <jana.heyn@web.de>
2023-07-02 22:37:31 +0200

    first cmmit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate} from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <>
       <nav>
-        <Link to="home">Home</Link>
-        <Link to="about">About</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
 
       <main>
         <Routes>
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={ <Contact /> }/>
         </Routes>
       </main>
     </>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+
+    const navigate = useNavigate();
+    return(
+        <>
+            <h1>Contact</h1>;
+            <button onClick={ () => navigate('/home')}>Submit</button>
+        </>
+    )
+
 };
 
 export default Contact;
